6f77a96d114c0641ff7ca87314adf5f28e862552
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-05-25 00:20:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ function App() {
     <>
       <div><Pfc name="RPFC" /></div>
       <div><Rcc name="RPCC"/></div>
-    <div>
+     <div>
+    <b> Hgher order functions </b>
       <p>A higher-order component is a function that takes a component and returns a new component.</p>
       <p>The purpose of a HOC is to enhance a component (usually a dumb component) with extra functionality. </p>
       <p>A HOC allows for reusability since in real-life applications there is a need to re-use the same functionalities in various similar kinds of components. A very common functionality is toggling.</p>
       </div>
+  <div><b>Pure Functions  </b>Such functions are called “pure” because they do not attempt to change their inputs, and always return the same result for the same inputs. </div>
     </>
   );
 }
